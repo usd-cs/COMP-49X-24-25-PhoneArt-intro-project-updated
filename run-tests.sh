f8508bbdc07d686e766d084f429f96e05103e3e8
AdityaPrakash2 <adityaprakash2003.ap@gmail.com>
2024-11-17 14:04:15 -0800

    Updating test file.

--- run-tests.sh
+++ run-tests.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 SCHEME='COMP-49X-24-25-PhoneArt-intro-project-updated'
 DESTINATION='platform=iOS Simulator,OS=18.1,name=iPhone 16'
-xcodebuild -project COMP-49X-24-25-PhoneArt-intro-project-updated.xcodeproj -scheme $SCHEME -destination "$DESTINATION" build test CODE_SIGNING_ALLOWED='NO'
+xcodebuild -project COMP-49X-24-25-PhoneArt-intro-project-updated.xcodeproj -scheme $SCHEME -destination "$DESTINATION" test CODE_SIGNING_ALLOWED='NO'
 killall "iOS Simulator" 2>/dev/null || true
